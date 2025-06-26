c162ef3d9e5d9688455f1b2845162078063f694b
farizanaufall <farizanaufal71@gmail.com>
2025-06-26 18:38:04 +0700

    keranjang kuning

--- script.js
+++ script.js
@@ -16,11 +16,28 @@ document.querySelector('#search-button').onclick = (e) => {
     e.preventDefault();
 };
 
-// klik diluar side bar untuk menghilangkan nav
-const hamburger = document.querySelector("#hamburger-menu");
+// Toggle class active untuk shopping cart
+const shoppingCart = document.querySelector('.shopping-cart');
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+  shoppingCart.classList.toggle('active');
+  e.preventDefault();
+};
 
-document.addEventListener("click" , function(e) {
-if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
-    navbarNav.classList.remove("active");
-}
+// Klik di luar elemen
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
+const sc = document.querySelector('#shopping-cart-button');
+
+document.addEventListener('click', function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove('active');
+  }
+
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove('active');
+  }
 });
