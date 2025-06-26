f276fe5523a798dc97d752b69f3899559e54f1ac
farizanaufall <farizanaufal71@gmail.com>
2025-06-26 18:00:07 +0700

    search button

--- script.js
+++ script.js
@@ -6,6 +6,16 @@ document.querySelector("#hamburger-menu").onclick = () => {
     navbarNav.classList.toggle("active");
 };
 
+// toggle class active search icon
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+};
+
 // klik diluar side bar untuk menghilangkan nav
 const hamburger = document.querySelector("#hamburger-menu");
 
