29e24ac7c4c1073f1d98b718d13328fd5fa837a2
farizanaufall <farizanaufal71@gmail.com>
2025-07-10 23:27:02 +0700

    masking

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Toggle class active
 const navbarNav = document.querySelector(".navbar-nav");
-
+ 
 // Ketika hamburger menu diklik
 document.querySelector("#hamburger-menu").onclick = () => { 
     navbarNav.classList.toggle("active");
